1032d52c6d43a2c74d29639788bbb14619e415da
Maneshwar Singh <63957278+maneshwarS@users.noreply.github.com>
2021-02-11 11:53:04 +0530

    Add files via upload

---
+++ face_recognition/face_recg.py
@@ -0,0 +1,47 @@
+import cv2
+import os
+import pickle
+
+face_cascade= cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
+eye_cascade= cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+recognizer= cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+
+labels={"persons_name":1}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v: k for k, v in og_labels.items()}
+
+cap= cv2.VideoCapture(0)
+while True:
+    ret, frame= cap.read()
+    gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces= face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in faces:
+        #print(x,y,w,h)
+        roi_gray=gray[y:y+h, x:x+w]
+        roi_color=frame[y:y+h, x:x+w]
+        id_, conf= recognizer.predict(roi_gray)
+        if conf>=4 and conf<=85:
+            print(id_)
+            print(labels[id_])
+            font= cv2.FONT_HERSHEY_SIMPLEX
+            name= labels[id_]
+            color= (255, 255, 255)
+            stroke=2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+        img_item="12.jpeg"
+        cv2.imwrite(img_item, roi_color)
+        color= (0, 255, 0)
+        stroke=2
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, stroke)
+        eyes= eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
